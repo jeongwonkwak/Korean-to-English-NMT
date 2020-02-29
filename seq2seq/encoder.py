9a5fe0ccceb59f0346a0bed41f8ef703885cc6db
jeongwonKwak <37138563+jeongwonkwak@users.noreply.github.com>
2020-02-29 11:08:47 +0900

    Create encoder.py

---
+++ seq2seq/encoder.py
@@ -0,0 +1,46 @@
+class Encoder(nn.Module):
+   
+    def __init__(self, word_vec_dim, hidden_size, n_layers=4, dropout_p=.2):
+        super(Encoder, self).__init__()
+
+        # Be aware of value of 'batch_first' parameter.
+        # Also, its hidden_size is half of original hidden_size,
+        # because it is bidirectional.
+        self.rnn = nn.LSTM(word_vec_dim,
+                           int(hidden_size / 2),
+                           num_layers=n_layers,
+                           dropout=dropout_p,
+                           bidirectional=True,
+                           batch_first=True
+                           )
+
+    def forward(self, emb):
+        # |emb| = (batch_size, length, word_vec_dim)
+
+        if isinstance(emb, tuple):
+            x, lengths = emb
+            x = pack(x, lengths.tolist(), batch_first=True)
+
+            # Below is how pack_padded_sequence works.
+            # As you can see,
+            # PackedSequence object has information about mini-batch-wise information,
+            # not time-step-wise information.
+            # 
+            # a = [torch.tensor([1,2,3]), torch.tensor([3,4])]
+            # b = torch.nn.utils.rnn.pad_sequence(a, batch_first=True)
+            # >>>>
+            # tensor([[ 1,  2,  3],
+            #     [ 3,  4,  0]])
+            # torch.nn.utils.rnn.pack_padded_sequence(b, batch_first=True, lengths=[3,2]
+            # >>>>PackedSequence(data=tensor([ 1,  3,  2,  4,  3]), batch_sizes=tensor([ 2,  2,  1]))
+        else:
+            x = emb
+
+        y, h = self.rnn(x)
+        # |y| = (batch_size, length, hidden_size)
+        # |h[0]| = (num_layers * 2, batch_size, hidden_size / 2)
+
+        if isinstance(emb, tuple):
+            y, _ = unpack(y, batch_first=True)
+
+        return y, h
