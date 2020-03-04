2723ee24219bcf5c88e3a3588413a7d6093bd737
Kwak Jeong Won <37138563+jeongwonkwak@users.noreply.github.com>
2020-03-04 10:25:54 +0900

    Update and rename seq2seq/Decoder.py to simple_nmt/decoder.py

---
+++ simple_nmt/decoder.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence as pack
+from torch.nn.utils.rnn import pad_packed_sequence as unpack
+
+
+class Decoder(nn.Module):
+
+    def __init__(self, word_vec_dim, hidden_size, n_layers=4, dropout_p=.2):
+        super(Decoder, self).__init__()
+
+        # Be aware of value of 'batch_first' parameter and 'bidirectional' parameter.
+        self.rnn = nn.LSTM(word_vec_dim + hidden_size,
+                           hidden_size,
+                           num_layers=n_layers,
+                           dropout=dropout_p,
+                           bidirectional=False,
+                           batch_first=True
+                           )
+
+    def forward(self, emb_t, h_t_1_tilde, h_t_1):
+        # |emb_t| = (batch_size, 1, word_vec_dim)
+        # |h_t_1_tilde| = (batch_size, 1, hidden_size)
+        # |h_t_1[0]| = (n_layers, batch_size, hidden_size)
+        batch_size = emb_t.size(0)
+        hidden_size = h_t_1[0].size(-1)
+
+        if h_t_1_tilde is None:
+            # If this is the first time-step,
+            h_t_1_tilde = emb_t.new(batch_size, 1, hidden_size).zero_()
+
+        # Input feeding trick.
+        x = torch.cat([emb_t, h_t_1_tilde], dim=-1)
+
+        # Unlike encoder, decoder must take an input for sequentially.
+        y, h = self.rnn(x, h_t_1)
+
+        return y, h
+
+class Generator(nn.Module):
+
+    def __init__(self, hidden_size, output_size):
+        super(Generator, self).__init__()
+
+        self.output = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=-1)
+
+    def forward(self, x):
+        # |x| = (batch_size, length, hidden_size)
+
+        y = self.softmax(self.output(x))
+        # |y| = (batch_size, length, output_size)
+
+        # Return log-probability instead of just probability.
+        print("***********generator****************")
+        return y
+
